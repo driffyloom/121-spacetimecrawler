d240e7b9cffc6d8a50916c8749573ac63914af1f
Bianca Tang <tang.bianca13@gmail.com>
2018-11-10 16:08:08 -0800

    Bug fixing

--- applications/search/crawler_frame.py
+++ applications/search/crawler_frame.py
@@ -9,7 +9,7 @@ from uuid import uuid4
 
 from urlparse import urlparse, parse_qs, urljoin
 from uuid import uuid4
-import datetime
+from datetime import datetime
 from collections import defaultdict
 
 
